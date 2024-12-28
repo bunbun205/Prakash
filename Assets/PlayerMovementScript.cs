3f2aeb6e4e6e556bc67fd1923147685cc19b2e54
bunbun205 <mayank69123@gmail.com>
2024-12-28 12:41:10 +0530

    faces

--- Assets/PlayerMovementScript.cs
+++ Assets/PlayerMovementScript.cs
@@ -1,6 +1,8 @@
 using UnityEngine;
 
-public class TargetController : MonoBehaviour
+public class PlayerMovementScript : MonoBehaviour
+
+public float speed = 6;
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
